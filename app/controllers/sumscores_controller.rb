82c7053bb7af10cbfedcc7cfa9cd51b27ef32228
momomouka <0717k.kenji@gmail.com>
2017-11-20 15:32:26 +0900

    1120-4

--- app/controllers/sumscores_controller.rb
+++ app/controllers/sumscores_controller.rb
@@ -35,6 +35,26 @@ class SumscoresController < ApplicationController
 	end
 
 	def create
+		@atmtresults = Atmtresult.where(member_id: @current_member.id).where.not(atmtTaskResult: nil)
+		@mathresults = Mathresult.where(member_id: @current_member.id).where.not(mathTaskResult: nil)
+		@clickresults = Clickresult.where(member_id: @current_member.id).where.not(clickTaskResult: nil)
+
+		$atmtsum = 0
+		$mathsum = 0
+		$clicksum = 0
+
+		@atmtresults.each do |member|
+			$atmtsum += member.atmtTaskResult
+		end
+
+		@mathresults.each do |member|
+			$mathsum += member.mathTaskResult
+		end
+
+		@clickresults.each do |member|
+			$clicksum += member.clickTaskResult
+		end
+
 		@sumscore = Sumscore.new
 		@sumscore.atmt = $atmtsum
 		@sumscore.math = $mathsum
@@ -45,6 +65,26 @@ class SumscoresController < ApplicationController
 	end
 
 	def update
+		@atmtresults = Atmtresult.where(member_id: @current_member.id).where.not(atmtTaskResult: nil)
+		@mathresults = Mathresult.where(member_id: @current_member.id).where.not(mathTaskResult: nil)
+		@clickresults = Clickresult.where(member_id: @current_member.id).where.not(clickTaskResult: nil)
+
+		$atmtsum = 0
+		$mathsum = 0
+		$clicksum = 0
+
+		@atmtresults.each do |member|
+			$atmtsum += member.atmtTaskResult
+		end
+
+		@mathresults.each do |member|
+			$mathsum += member.mathTaskResult
+		end
+
+		@clickresults.each do |member|
+			$clicksum += member.clickTaskResult
+		end
+
 		@sumscore = Sumscore.find_by(member_id: @current_member.id)
 		@sumscore.update(atmt:$atmtsum,math:$mathsum,click:$clicksum)
 		redirect_to '/tasks/index'
