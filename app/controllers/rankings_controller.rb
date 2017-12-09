f22094a7c2d539d7e1e814f7cd35e44dfcd0158e
momomouka <0717k.kenji@gmail.com>
2017-12-09 11:41:34 +0900

    1208-2

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -25,9 +25,9 @@ class RankingsController < ApplicationController
     @count = @member.ranking + 1
     @member = Member.find(@current_member.id).update!(ranking: @count)
 
-    @rank_atmt = Member.where(tasktype: 1, gametype: 2).or(Member.where(tasktype: 4, gametype: 4)).order("atmt desc").where.not(atmt: 0)
+    @rank_atmt = Member.where(tasktype: 1, gametype: 2).where.not(atmt: 0)
 
-    @rank_atmttoday = Member.where(tasktype: 1, gametype: 2).or(Member.where(tasktype: 4, gametype: 4)).order("atmttoday desc").where.not(atmttoday: 0)
+    @rank_atmttoday = Member.where(tasktype: 1, gametype: 2).where.not(atmttoday: 0)
 
   end
 
@@ -36,9 +36,9 @@ class RankingsController < ApplicationController
     @count = @member.ranking + 1
     @member = Member.find(@current_member.id).update!(ranking: @count)
 
-    @rank_math = Member.where(tasktype: 2, gametype: 2).or(Member.where(tasktype: 4, gametype: 4)).order("math desc").where.not(math: 0)
+    @rank_math = Member.where(tasktype: 2, gametype: 2).where.not(math: 0)
 
-    @rank_mathtoday = Member.where(tasktype: 2, gametype: 2).or(Member.where(tasktype: 4, gametype: 4)).order("mathtoday desc").where.not(mathtoday: 0)
+    @rank_mathtoday = Member.where(tasktype: 2, gametype: 2).where.not(mathtoday: 0)
   end
 
   def click
@@ -46,9 +46,9 @@ class RankingsController < ApplicationController
     @count = @member.ranking + 1
     @member = Member.find(@current_member.id).update!(ranking: @count)
 
-    @rank_click = Member.where(tasktype: 3, gametype: 2).or(Member.where(tasktype: 4, gametype: 4)).order("click desc").where.not(click: 0)
+    @rank_click = Member.where(tasktype: 3, gametype: 2).where.not(click: 0)
 
-    @rank_clicktoday = Member.where(tasktype: 3, gametype: 2).or(Member.where(tasktype: 4, gametype: 4)).order("clicktoday desc").where.not(clicktoday: 0)
+    @rank_clicktoday = Member.where(tasktype: 3, gametype: 2).where.not(clicktoday: 0)
 
   end
 
