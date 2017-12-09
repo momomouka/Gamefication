3b6fee1af2374dafaa0d0e8d412bdb6a3dd9613c
momomouka <0717k.kenji@gmail.com>
2017-12-09 11:50:29 +0900

    1208-2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
   include SessionsHelper
   
     # before_actionにset_current_userメソッドを指定
